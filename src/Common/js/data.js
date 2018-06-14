c1dc3422734e9da1e40c06480a05212409cadc36
JasonLiu001 <wangliu@isoftstone.com>
2018-06-14 16:35:51 +0800

    明细列表展示修改

--- src/Common/js/data.js
+++ src/Common/js/data.js
@@ -1,5 +1,5 @@
 /*列表中表字段*/
-const tableFields = ['field1', 'field2', 'field3', 'field4', 'field5', 'field6', 'field7'];
+const tableFields = ['field1', 'field2', 'field3', 'field4', 'field5', 'field6', 'field7', 'field8'];
 
 export {
     tableFields
