1fd2f069be2a4491b4f0cd512df1b7ec53eaec58
JasonLiu001 <wangliu@isoftstone.com>
2018-06-14 21:01:09 +0800

    页面参数传递

--- src/Common/js/data.js
+++ src/Common/js/data.js
@@ -1,6 +1,64 @@
 /*列表中表字段*/
 const tableFields = ['field1', 'field2', 'field3', 'field4', 'field5', 'field6', 'field7', 'field8'];
 
+const investTable = [
+    {
+        name: 'period',
+        des: 'period'
+    },
+    {
+        name: 'planType',
+        des: 'planType'
+    },
+    {
+        name: 'investNumberCount',
+        des: 'investNumberCount'
+    },
+    {
+        name: 'currentAccountBalance',
+        des: 'currentAccountBalance'
+    },
+    {
+        name: 'STATUS',
+        des: 'STATUS'
+    },
+    {
+        name: 'isWin',
+        des: 'isWin'
+    },
+    {
+        name: 'investTime',
+        des: 'investTime'
+    }];
+
+
+const maxProfitTable = [
+    {
+        name: 'period',
+        des: 'period'
+    },
+    {
+        name: 'planType',
+        des: 'planType'
+    },
+    {
+        name: 'originAccoutBalance',
+        des: 'originAccoutBalance'
+    }, {
+        name: 'maxAccountBalance',
+        des: 'maxAccountBalance'
+    },
+    {
+        name: 'isRealInvest',
+        des: 'isRealInvest'
+    }, {
+        name: 'createTime',
+        des: 'createTime'
+    }
+];
+
 export {
-    tableFields
+    tableFields,
+    investTable,
+    maxProfitTable
 }
