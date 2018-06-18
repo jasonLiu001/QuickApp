5f2f75b6cd72e6b196af1b76175acec0a4afc55c
JasonLiu001 <wangliu@isoftstone.com>
2018-06-18 19:09:16 +0800

    添加数据获取

--- src/Common/js/data.js
+++ src/Common/js/data.js
@@ -1,64 +1,74 @@
 /*列表中表字段*/
 const tableFields = ['field1', 'field2', 'field3', 'field4', 'field5', 'field6', 'field7', 'field8'];
 
+const apiUrl = {
+    findAwardInfoList: 'https://www.cnlands.cn/servie/lottery/findAwardInfoList',
+    findInvestInfoList: 'https://www.cnlands.cn/servie/lottery/findInvestInfoList',
+    findMaxProfitInfoList: 'https://www.cnlands.cn/servie/lottery/findMaxProfitInfoList',
+    findPlanInfoList: 'https://www.cnlands.cn/servie/lottery/findPlanInfoList',
+    findPlanResultInfoList: 'https://www.cnlands.cn/servie/lottery/findPlanResultInfoList',
+    findInvestNumbersInfoList: 'https://www.cnlands.cn/servie/lottery/findInvestNumbersInfoList'
+};
+
 const investTable = [
     {
         name: 'period',
-        des: 'period'
+        des: '期号'
     },
     {
         name: 'planType',
-        des: 'planType'
+        des: '计划类型'
     },
     {
         name: 'investNumberCount',
-        des: 'investNumberCount'
+        des: '购买号码数量'
     },
     {
         name: 'currentAccountBalance',
-        des: 'currentAccountBalance'
+        des: '当前账号余额'
     },
     {
         name: 'STATUS',
-        des: 'STATUS'
+        des: '开奖状态'
     },
     {
         name: 'isWin',
-        des: 'isWin'
+        des: '是否中奖'
     },
     {
         name: 'investTime',
-        des: 'investTime'
+        des: '购买号码时间'
     }];
 
 
 const maxProfitTable = [
     {
         name: 'period',
-        des: 'period'
+        des: '期号'
     },
     {
         name: 'planType',
-        des: 'planType'
+        des: '计划类型'
     },
     {
         name: 'originAccoutBalance',
-        des: 'originAccoutBalance'
+        des: '初始账号余额'
     }, {
         name: 'maxAccountBalance',
-        des: 'maxAccountBalance'
+        des: '最大盈利值'
     },
     {
         name: 'isRealInvest',
-        des: 'isRealInvest'
+        des: '是否是真实投注'
     }, {
         name: 'createTime',
-        des: 'createTime'
+        des: '购买号码时间'
     }
 ];
 
 export {
     tableFields,
     investTable,
-    maxProfitTable
+    maxProfitTable,
+    apiUrl
 }
